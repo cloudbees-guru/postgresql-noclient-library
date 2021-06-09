b1ea1497aa57ac78102f284a266a2cc4c384cc69
Jean-Philippe Briend <jeanphilippe.briend@gmail.com>
2021-06-09 15:32:20 +0200

    Handle multiple queries as query parameter

--- src/main/java/com/cloudbees/cd/plugin/postgresql/PostgreSQLConnection.java
+++ src/main/java/com/cloudbees/cd/plugin/postgresql/PostgreSQLConnection.java
@@ -1,6 +1,12 @@
 package com.cloudbees.cd.plugin.postgresql;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Hello world!
@@ -13,15 +19,34 @@ public class PostgreSQLConnection {
     private String login;
     private String password;
     private String url_start = "jdbc:postgresql://";
+    private final String separator = "\n;";
+
+    private List<String> getQueries(String cd_parameter) {
+        if (cd_parameter == null) {
+            return Collections.emptyList();
+        } else {
+            return Arrays.asList(cd_parameter.split(separator));
+        }
+    }
 
     public void executeQuery(String query) throws Exception {
+        List<String> queries = this.getQueries(query);
         Connection con = this.connect();
         Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery(query);
-        if (rs.next()) {
-            System.out.println(rs.getString(1));
+        con.setAutoCommit(false);
+
+        try {
+            for (String q : queries) {
+                st.addBatch(q);
+            }
+            st.executeBatch();
+            con.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new Exception("Error while executing the queries: " + e.getMessage(), e);
+        } finally {
+            this.disconnect(con);
         }
-        this.disconnect(con);
     }
 
     private Connection connect() throws Exception {
